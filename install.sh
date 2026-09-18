7cc306a62ccfc76e0e3231bf107bdd5f430a9076
Vamora1 <aniserri.vamora@gmail.com>
2026-09-18 18:55:17 +0100

    PreAlpha 3

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+sudo lb clean --purge
+
+sudo lb config \
+  --distribution trixie \
+  --archive-areas "main contrib non-free non-free-firmware" \
+  --debian-installer none \
+  --binary-images iso-hybrid \
+  --bootappend-live "boot=live components hostname=vamoraos quiet splash"
+
+echo Building ISO...
+
+sudo lb build
